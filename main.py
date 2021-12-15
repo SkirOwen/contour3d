7283b8e9bb1d416d880beba1736020d9aae77d9c
Owen <owen.allemang@gmail.com>
2021-12-15 13:38:52 +0000

    init commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import sys
+
+import PIL
+
+
+def get_palette(max_height: int, min_height: int, contour_dist: int) -> None:
+	pass
+
+
+def load_img() -> None:
+	pass
+
+
+def parse_contour(img, palette) -> None:
+	pass
+
+
+def plot_3d(contour_parser) -> None:
+	pass
+
+
+def main():
+	max_h, min_h, c_d = 0, 0, 0
+	palette = get_palette(max_h, min_h, c_d)
+
+	img = load_img()
+
+
+if __name__ == '__main__':
+	main()
